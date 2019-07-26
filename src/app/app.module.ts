e363a492bac0107f97fb5eedabd1cc1956444510
Shipov Mikhail <mshipov@yandex.ru>
2019-07-26 15:50:07 +0300

    changes from test task with fixes for latest angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,72 @@
+import { Component, NgModule, Input, OnInit, OnChanges } from '@angular/core';
+import * as Rx from 'rxjs';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ApiService } from './ApiService';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+@Component({
+  // tslint:disable-next-line
+  selector: '[header]',
+  template: `
+    <th>№</th>
+    <th>Name</th>
+    <th>Score</th>
+    <th>Note</th>
+  `
+})
+export class HeaderComponent {}
+
+@Component({
+  // tslint:disable-next-line
+  selector: '[item-detail]',
+  template: `
+    <td>{{item.id}}</td>
+    <td>{{item.name}}</td>
+    <td>{{item.score}}</td>
+    <td><input size="7" /></td>
+  `
+})
+export class RowComponent implements OnChanges {
+  @Input() item;
+  ngOnChanges() {
+    console.log('inputs changes');
+  }
+}
+
+@Component({
+  selector: 'app-component',
+  providers: [ApiService],
+  template: `<table border="2" cellpadding="5">
+    <thead>
+      <tr header></tr>
+    </thead>
+    <tbody>
+      <tr *ngFor="let item of items" item-detail [item]='item'></tr>
+    </tbody>
+  </table>`
+})
+export class AppComponent implements OnInit {
+  items: {
+    name: string,
+    score: number
+  }[];
+  constructor(private apiService: ApiService) {}
+  ngOnInit() {
+    this.apiService.handleChanges(changes => {
+      this.items = this.calcScore(changes);
+    });
+  }
+
+  calcScore(items) {
+    return items.map(item => ({
+      ...item,
+      score: Math.round(item.rate / 100 * 5)
+    }));
+  }
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
+  declarations: [AppComponent, RowComponent, HeaderComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
